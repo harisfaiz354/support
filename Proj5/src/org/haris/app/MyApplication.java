9c6649a6f13ac873e9cd0ac31aec1c23d1f2e6c8
haris <haris@example.com>
2019-02-21 14:42:59 -0500

    first commit

---
+++ Proj5/src/org/haris/app/MyApplication.java
@@ -0,0 +1,13 @@
+package org.haris.app;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author Administrator
+ *
+ */
+@ApplicationPath("/app")
+public class MyApplication extends Application{
+
+}
---
+++ Proj5/src/org/haris/service/CustomerService.java
@@ -0,0 +1,36 @@
+package org.haris.service;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+
+@Path("/customer/service")
+public class CustomerService {
+     @GET
+	 public void getCustomer()
+	 {
+		 System.out.println("CustomerService.getCustomer()");
+	 }
+     
+     @PUT
+     @Path("/create")
+     public void createCustomer()
+     {
+    	 System.out.println("CustomerService.createCustomer()");
+     }
+     
+     @POST
+     public void updateUser()
+     {
+    	 System.out.println("CustomerService.updateUser()");
+     }
+     
+     @DELETE
+     @Path("/delete")
+     public void deleteUser()
+     {
+    	 System.out.println("CustomerService.deleteUser()");
+     }
+}
